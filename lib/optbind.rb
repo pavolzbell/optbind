7c2b695522c637d68e506699c0847c2292308df2
Pavol Zbell <pavol.zbell@gmail.com>
2015-12-25 00:33:01 +0100

    Add initial draft

--- lib/optbind.rb
+++ lib/optbind.rb
@@ -1,4 +1,202 @@
-module OptionBinder
+require 'forwardable'
+require 'optparse'
+
+class OptionBinder
+  attr_reader :parser, :target
+
+  def initialize(parser: nil, target: TOPLEVEL_BINDING, locals: false)
+    @parser = resolve_parser(parser)
+    @target, @reader, @writer = target, resolve_binding(target, locals)
+  end
+
+  def resolve_binding(target, locals = false)
+    return -> (v) { target[v] }, -> (v, x) { target[v] = x } if target.respond_to? :[]
+    return -> (v) { target.public_send v }, -> (v, x) { target.public_send v, x } unless target.is_a? Binding
+    return -> (v) { target.local_variable_get v }, -> (v, x) { target.local_variable_set v, x } if locals
+    return -> (v) { target.instance_variable_get v }, -> (v, x) { target.instance_variable_set v, x }
+  end
+
+  def resolve_parser(parser = nil)
+    parser || OptionParser.new do |p|
+      p.on_tail '-h', '--help' #TODO { abort to_s }
+      p.on_tail '--version'
+    end
+  end
+
+  private :resolve_binding, :resolve_parser
+
+  extend Forwardable
+
+  def_delegators :@parser, :accept, :reject
+  def_delegators :@parser, :abort, :warn
+  def_delegators :@parser, :load
+
+  def parse(*argv)
+    super
+    parse_args *argv
+  end
+
+  def parse!(*argv)
+    super
+    parse_args! *argv
+  end
+
+  def_delegators :@parser, :to_a, :to_s
+
+  def_delegator :@parser, :program_name, :program
+  def_delegator :@parser, :version
+
+  def usage(*args)
+    line = (args * ' ') << "\n"
+
+    if @parser.banner.nil?
+      @parser.on_head "\n" and @parser.on_tail "\n"
+      @parser.banner << "usage: #{program} " << line
+    else
+      @parser.banner << "   or: #{program} " << line
+    end
+
+    self
+  end
+
+  alias_method :use, :usage
+
+  def option(*opts, &handler)
+    bound = false
+
+    if opts.size == 1
+      case opts[0]
+      when Hash then
+        hash, variable = opts[0], [hash.delete(:variable), hash.delete(:bind)].compact[0]
+        bound, default = !!variable, hash.delete(:default) || (@reader.call(bound) if bound)
+        opts, handler = Switch.parser_opts_from_hash hash, &handler
+      when String then
+        string, variable = opts[0] =~ /\A-/ ? opts[0].split(/\s+/, 2).reverse : opts[0], nil
+        bound, default = !!variable, (@reader.call(bound) if bound)
+        opts, handler = Switch.parser_opts_from_string string, &handler
+      end
+    end
+
+    (@bound_variables_with_defaults ||= {})[variable] = default if bound
+
+    @parser.on(opts) do |r|
+      unless opts.include? :OPTIONAL
+        a = opts.select { |o| o =~ /\A-/ }.sort_by { |o| o.length }[-1]
+        @parser.abort "missing argument: #{a}=" if !r || (r.respond_to?(:empty?) && r.empty?)
+      end
+
+      (handler || -> (_) { r }).call(r == nil ? default : r).tap { |x| @writer.call variable, x if bound }
+    end
+
+    self
+  end
+
+  alias_method :opt, :option
+
+  #TODO
+  def argument(*args, &handler)
+
+    self
+  end
+
+  alias_method :arg, :argument
+
+  def bound_defaults
+    @bound_variables_with_defaults ? @bound_variables_with_defaults.dup : {}
+  end
+
+  def bound_variables
+    return {} unless @bound_variables_with_defaults
+    Hash[@bound_variables_with_defaults.keys.map { |v| [v, @reader.call(v)] }]
+  end
+
+  def default?(v)
+    return nil unless (@bound_variables_with_defaults || {}).has_key? v
+    @bound_variables_with_defaults[v] == @reader.call(v)
+  end
+
+  module Switch
+    def self.parser_opts_from_hash(hash = {}, &handler)
+      style = case (hash[:style] || hash[:mode]).to_s.downcase
+      when 'required' then :REQUIRED
+      when 'optional' then :OPTIONAL
+      end
+
+      pattern = hash[:pattern] || hash[:type]
+      values = hash[:values]
+
+      names = [hash[:long], hash[:longs]].flatten.map { |n| n.to_s.sub(/\A-{,2}/, '--') if n }
+      names += [hash[:short], hash[:shorts]].flatten.map { |n| n.to_s.sub(/\A-{,2}/, '-') if n }
+      names += [hash[:name], hash[:names]].flatten.map do |n|
+        next unless n
+        n = n.to_s
+        next n if n[0] == '-'
+        n[2] ? "--#{n}" : "-#{n}"
+      end
+
+      argument = (hash[:argument].to_s if hash[:argument])
+      description = ([hash[:description]].flatten * ' ' if hash[:description])
+      handler ||= hash[:handler]
+
+      return ([style, pattern, values] + names + [argument, description]).compact, handler
+    end
+
+    def self.parser_opts_from_string(string = '', &handler)
+      shorts, longs = [], []
+
+      while string.sub!(/\A(?:(?<short>-\w)\s+)/, '')
+        shorts << $~[:short]
+      end
+
+      style, pattern, values, argument = nil
+
+      while string.sub!(/\A(?:(?<long>--[\[\]\-\w]+[\]\w]+)?(?:(?<argument>\[?=[<(]\S+[)>]\]?)|\s+))/, '')
+        longs << $~[:long]
+        argument = $~[:argument]
+
+        next unless argument
+
+        style = argument[0] == '=' ? :REQUIRED : :OPTIONAL
+        values = $~[:values].split('|') if argument =~ /\[?=\((?<values>\S*)\)\]?/
+
+        if values.nil? && argument =~ /\[?=<(?<name>\S+):(?<pattern>\S+)>\]?/
+          pattern = Module.const_get($~[:pattern]) rescue Regexp.new($~[:pattern])
+          argument = "=<#{$~[:name]}>"
+          argument = "[#{argument}]" if style == :OPTIONAL
+        end
+      end
+
+      description = !string.empty? ? string.strip : nil
+
+      return ([style, pattern, values] + shorts + longs + [argument, description]).compact, handler
+    end
+  end
+
+  #TODO
+  def parse_args(*argv)
+
+  end
+
+  private :parse_args
+
+  #TODO
+  def parse_args!(*argv)
+
+  end
+
+  private :parse_args!
+
+  #TODO
+  module Arguable
+    def define_and_bind(to: TOPLEVEL_BINDING, locals: false)
+      @optbind = OptionBinder.new parser: nil, target: to, locals: locals
+      self.options = @optbind.parser
+      yield @optbind if block_given?
+      @optbind
+    end
+  end
 end
+
+ARGV.extend(OptionBinder::Arguable)
 
 OptBind = OptionBinder
---
+++ spec/optbind/switch_spec.rb
@@ -0,0 +1,242 @@
+require 'spec_helper'
+
+describe OptBind::Switch do
+  describe '#parser_opts_from_hash' do
+    context 'with no arguments' do
+      it 'returns nothing' do
+        expect(subject.parser_opts_from_hash).to contain_exactly [], nil
+        expect(subject.parser_opts_from_hash({})).to contain_exactly [], nil
+      end
+    end
+
+    context 'with style or mode' do
+      it 'returns style' do
+        expect(subject.parser_opts_from_hash style: :REQUIRED).to contain_exactly [:REQUIRED], nil
+        expect(subject.parser_opts_from_hash mode: :OPTIONAL).to contain_exactly [:OPTIONAL], nil
+
+        expect(subject.parser_opts_from_hash style: :OPTIONAL, mode: :REQUIRED).to contain_exactly [:OPTIONAL], nil
+        expect(subject.parser_opts_from_hash mode: :OPTIONAL, style: :REQUIRED).to contain_exactly [:REQUIRED], nil
+      end
+
+      it 'ignores case' do
+        expect(subject.parser_opts_from_hash style: :required).to contain_exactly [:REQUIRED], nil
+      end
+
+      it 'handles strings' do
+        expect(subject.parser_opts_from_hash style: 'REQUIRED').to contain_exactly [:REQUIRED], nil
+        expect(subject.parser_opts_from_hash mode: 'OPTIONAL').to contain_exactly [:OPTIONAL], nil
+      end
+    end
+
+    context 'with pattern or type' do
+      it 'returns pattern' do
+        expect(subject.parser_opts_from_hash pattern: //).to contain_exactly [//], nil
+        expect(subject.parser_opts_from_hash type: Object).to contain_exactly [Object], nil
+
+        expect(subject.parser_opts_from_hash pattern: //, type: Object).to contain_exactly [//], nil
+        expect(subject.parser_opts_from_hash type: Object, pattern: //).to contain_exactly [//], nil
+      end
+    end
+
+    context 'with values' do
+      it 'returns values' do
+        expect(subject.parser_opts_from_hash values: %w(on off)).to contain_exactly [%w(on off)], nil
+        expect(subject.parser_opts_from_hash values: { on: 1, off: 0 }).to contain_exactly [{ on: 1, off: 0 }], nil
+      end
+    end
+
+    context 'with long or longs' do
+      it 'returns longs' do
+        expect(subject.parser_opts_from_hash long: '--v').to contain_exactly %w(--v), nil
+        expect(subject.parser_opts_from_hash long: '--verbose').to contain_exactly %w(--verbose), nil
+        expect(subject.parser_opts_from_hash longs: %w(--v --verbose)).to contain_exactly %w(--v --verbose), nil
+      end
+
+      it 'fixes shorts and unknowns' do
+        expect(subject.parser_opts_from_hash long: 'v').to contain_exactly %w(--v), nil
+        expect(subject.parser_opts_from_hash long: '-v').to contain_exactly %w(--v), nil
+
+        expect(subject.parser_opts_from_hash long: 'verbose').to contain_exactly %w(--verbose), nil
+        expect(subject.parser_opts_from_hash long: '-verbose').to contain_exactly %w(--verbose), nil
+
+        expect(subject.parser_opts_from_hash longs: %w(v s)).to contain_exactly %w(--v --s), nil
+        expect(subject.parser_opts_from_hash longs: %w(-v -s)).to contain_exactly %w(--v --s), nil
+
+        expect(subject.parser_opts_from_hash longs: %w(verbose silent)).to contain_exactly %w(--verbose --silent), nil
+        expect(subject.parser_opts_from_hash longs: %w(-verbose -silent)).to contain_exactly %w(--verbose --silent), nil
+      end
+
+      it 'handles symbols' do
+        expect(subject.parser_opts_from_hash longs: %i(v s)).to contain_exactly %w(--v --s), nil
+        expect(subject.parser_opts_from_hash longs: %i(verbose silent)).to contain_exactly %w(--verbose --silent), nil
+      end
+    end
+
+    context 'with short or shorts' do
+      it 'returns shorts' do
+        expect(subject.parser_opts_from_hash short: '-v').to contain_exactly %w(-v), nil
+        expect(subject.parser_opts_from_hash short: '-verbose').to contain_exactly %w(-verbose), nil
+        expect(subject.parser_opts_from_hash shorts: %w(-v -verbose)).to contain_exactly %w(-v -verbose), nil
+      end
+
+      it 'fixes longs and unknowns' do
+        expect(subject.parser_opts_from_hash short: 'v').to contain_exactly %w(-v), nil
+        expect(subject.parser_opts_from_hash short: '--v').to contain_exactly %w(-v), nil
+
+        expect(subject.parser_opts_from_hash short: 'verbose').to contain_exactly %w(-verbose), nil
+        expect(subject.parser_opts_from_hash short: '--verbose').to contain_exactly %w(-verbose), nil
+
+        expect(subject.parser_opts_from_hash shorts: %w(v s)).to contain_exactly %w(-v -s), nil
+        expect(subject.parser_opts_from_hash shorts: %w(--v --s)).to contain_exactly %w(-v -s), nil
+
+        expect(subject.parser_opts_from_hash shorts: %w(verbose silent)).to contain_exactly %w(-verbose -silent), nil
+        expect(subject.parser_opts_from_hash shorts: %w(--verbose --silent)).to contain_exactly %w(-verbose -silent), nil
+      end
+
+      it 'handles symbols' do
+        expect(subject.parser_opts_from_hash shorts: %i(v s)).to contain_exactly %w(-v -s), nil
+        expect(subject.parser_opts_from_hash shorts: %i(verbose silent)).to contain_exactly %w(-verbose -silent), nil
+      end
+    end
+
+    context 'with name or names' do
+      it 'returns longs and shorts' do
+        expect(subject.parser_opts_from_hash name: '-v').to contain_exactly %w(-v), nil
+        expect(subject.parser_opts_from_hash name: '-verbose').to contain_exactly %w(-verbose), nil
+
+        expect(subject.parser_opts_from_hash name: '--v').to contain_exactly %w(--v), nil
+        expect(subject.parser_opts_from_hash name: '--verbose').to contain_exactly %w(--verbose), nil
+
+        expect(subject.parser_opts_from_hash names: %w(-v --verbose)).to contain_exactly %w(-v --verbose), nil
+      end
+
+      it 'fixes unknowns' do
+        expect(subject.parser_opts_from_hash name: 'v').to contain_exactly %w(-v), nil
+        expect(subject.parser_opts_from_hash name: 'verbose').to contain_exactly %w(--verbose), nil
+
+        expect(subject.parser_opts_from_hash names: %w(v verbose)).to contain_exactly %w(-v --verbose), nil
+      end
+
+      it 'handles symbols' do
+        expect(subject.parser_opts_from_hash names: %i(v verbose)).to contain_exactly %w(-v --verbose), nil
+      end
+    end
+
+    context 'with argument' do
+      it 'returns argument' do
+        expect(subject.parser_opts_from_hash argument: '=<path>').to contain_exactly %w(=<path>), nil
+        expect(subject.parser_opts_from_hash argument: '=[<path>]').to contain_exactly %w(=[<path>]), nil
+        expect(subject.parser_opts_from_hash argument: '[=<path>]').to contain_exactly %w([=<path>]), nil
+      end
+
+      it 'handles symbols' do
+        expect(subject.parser_opts_from_hash argument: :'=<path>').to contain_exactly %w(=<path>), nil
+      end
+    end
+
+    context 'with description' do
+      it 'returns description' do
+        expect(subject.parser_opts_from_hash description: 'Be more verbose').to contain_exactly ['Be more verbose'], nil
+      end
+
+      it 'handles array of strings' do
+        expect(subject.parser_opts_from_hash description: %w(Be more verbose)).to contain_exactly ['Be more verbose'], nil
+      end
+    end
+
+    context 'with handler or block' do
+      it 'returns handler' do
+        handler = -> {}
+
+        expect(subject.parser_opts_from_hash &handler).to contain_exactly [], handler
+        expect(subject.parser_opts_from_hash handler: handler).to contain_exactly [], handler
+        expect(subject.parser_opts_from_hash handler: -> {}, &handler).to contain_exactly [], handler
+      end
+    end
+  end
+
+  describe '#parser_opts_from_string' do
+    context 'with no arguments' do
+      it 'returns nothing' do
+        expect(subject.parser_opts_from_string).to contain_exactly [], nil
+        expect(subject.parser_opts_from_string '').to contain_exactly [], nil
+      end
+    end
+
+    context 'with shorts and longs' do
+      it 'returns shorts and longs' do
+        expect(subject.parser_opts_from_string '-v').to contain_exactly %w(-v), nil
+        expect(subject.parser_opts_from_string '-v -s').to contain_exactly %w(-v -s), nil
+
+        expect(subject.parser_opts_from_string '--verbose').to contain_exactly %w(--verbose), nil
+        expect(subject.parser_opts_from_string '--verbose --silent').to contain_exactly %w(--verbose --silent), nil
+
+        expect(subject.parser_opts_from_string '-v --verbose').to contain_exactly %w(-v --verbose), nil
+        expect(subject.parser_opts_from_string '-q -s --quiet --silent').to contain_exactly %w(-q -s --quiet --silent), nil
+        expect(subject.parser_opts_from_string '-q -s --[no-]quiet --[no-]silent').to contain_exactly %w(-q -s --[no-]quiet --[no-]silent), nil
+      end
+
+      context 'with argument' do
+        it 'returns shorts, longs, style, and argument' do
+          expect(subject.parser_opts_from_string '-f =<path>').to contain_exactly [:REQUIRED, '-f', '=<path>'], nil
+          expect(subject.parser_opts_from_string '-f --file =<path>').to contain_exactly [:REQUIRED, '-f', '--file', '=<path>'], nil
+          expect(subject.parser_opts_from_string '-f -p --file --path =<path>').to contain_exactly [:REQUIRED, '-f', '-p', '--file', '--path', '=<path>'], nil
+
+          expect(subject.parser_opts_from_string '-f [=<path>]').to contain_exactly [:OPTIONAL, '-f', '[=<path>]'], nil
+          expect(subject.parser_opts_from_string '-f --file [=<path>]').to contain_exactly [:OPTIONAL, '-f', '--file', '[=<path>]'], nil
+          expect(subject.parser_opts_from_string '-f -p --file --path [=<path>]').to contain_exactly [:OPTIONAL, '-f', '-p', '--file', '--path', '[=<path>]'], nil
+
+          expect(subject.parser_opts_from_string '-f --file=<path>').to contain_exactly [:REQUIRED, '-f', '--file', '=<path>'], nil
+          expect(subject.parser_opts_from_string '-f -p --file --path=<path>').to contain_exactly [:REQUIRED, '-f', '-p', '--file', '--path', '=<path>'], nil
+
+          expect(subject.parser_opts_from_string '-f --file[=<path>]').to contain_exactly [:OPTIONAL, '-f', '--file', '[=<path>]'], nil
+          expect(subject.parser_opts_from_string '-f -p --file --path[=<path>]').to contain_exactly [:OPTIONAL, '-f', '-p', '--file', '--path', '[=<path>]'], nil
+        end
+      end
+
+      context 'with description' do
+        it 'returns shorts, longs, and description' do
+          expect(subject.parser_opts_from_string '-v Be more verbose').to contain_exactly ['-v', 'Be more verbose'], nil
+          expect(subject.parser_opts_from_string '--verbose Be more verbose').to contain_exactly ['--verbose', 'Be more verbose'], nil
+        end
+      end
+    end
+
+    context 'with argument' do
+      context 'with type' do
+        it 'returns argument and type' do
+          expect(subject.parser_opts_from_string '=<value:Numeric>').to contain_exactly [:REQUIRED, Numeric, '=<value>'], nil
+          expect(subject.parser_opts_from_string '[=<value:Numeric>]').to contain_exactly [:OPTIONAL, Numeric, '[=<value>]'], nil
+        end
+      end
+
+      context 'with values' do
+        it 'returns argument and values' do
+          expect(subject.parser_opts_from_string '=(on|off)').to contain_exactly [:REQUIRED, %w(on off), '=(on|off)'], nil
+          expect(subject.parser_opts_from_string '[=(on|off)]').to contain_exactly [:OPTIONAL, %w(on off), '[=(on|off)]'], nil
+        end
+      end
+
+      context 'with regexp' do
+        it 'returns argument and regexp' do
+          expect(subject.parser_opts_from_string '=<indent:\d+>').to contain_exactly [:REQUIRED, /\d+/, '=<indent>'], nil
+          expect(subject.parser_opts_from_string '[=<indent:\d+>]').to contain_exactly [:OPTIONAL, /\d+/, '[=<indent>]'], nil
+        end
+      end
+    end
+
+    context 'with description' do
+      it 'returns description' do
+        expect(subject.parser_opts_from_string 'Be more verbose').to contain_exactly ['Be more verbose'], nil
+      end
+    end
+
+    context 'with handler or block' do
+      it 'returns handler' do
+        handler = -> {}
+
+        expect(subject.parser_opts_from_string &handler).to contain_exactly [], handler
+      end
+    end
+  end
+end
